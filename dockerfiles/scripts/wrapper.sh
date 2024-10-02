648307ebe8fb9e16dc475b434a4f57322eaa92ce
Fleming, Michael (NIH/NCI) [C] <michael.fleming@nih.gov>
2024-10-02 12:31:29 -0400

    updated for 4.0.0

--- dockerfiles/scripts/wrapper.sh
+++ dockerfiles/scripts/wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# seed the bento 50k data set
+# seed the bento 1k data set
 mkdir -p /data/databases/graph.db && bin/neo4j-admin load --from=bento-data.dump --force
 
 # Start the primary process
